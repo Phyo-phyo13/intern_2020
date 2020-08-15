69419b11e8b6f504ebba70dc4378c3244170594d
intern2020 <phyozinmar1312000@gmail.com>
2020-08-15 19:40:03 +0630

    spring tool

---
+++ week2/java assignment/myfirst/src/main/java/spring_boot/myfirstapp/App.java
@@ -0,0 +1,14 @@
+package spring_boot.myfirstapp;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
---
+++ week2/java assignment/myfirst/src/main/java/spring_boot/myfirstapp/SpringApplication.java
@@ -0,0 +1,10 @@
+package spring_boot.myfirstapp;
+
+public class SpringApplication {
+
+	public static void run(Class<App> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
