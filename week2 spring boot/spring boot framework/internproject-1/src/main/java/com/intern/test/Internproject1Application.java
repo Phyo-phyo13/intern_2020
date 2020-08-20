db9dc7364d05e94158a0434ec0f260b45db479e5
intern2020 <phyozinmar1312000@gmail.com>
2020-08-20 17:26:37 +0630

    spring boot for teacher

---
+++ week2 spring boot/spring boot framework/internproject-1/src/main/java/com/intern/test/Internproject1Application.java
@@ -0,0 +1,13 @@
+package com.intern.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Internproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Internproject1Application.class, args);
+	}
+
+}
